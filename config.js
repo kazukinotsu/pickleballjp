97c5d09329f595425053ddbc6c1d813451e9a734
kazukinotsu <49308743+kazukinotsu@users.noreply.github.com>
2026-04-25 12:28:30 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 /* Edit these two values (from Supabase > Settings > API), then redeploy. */
-window.SUPABASE_URL      = "YOUR_SUPABASE_URL";
-window.SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+window.SUPABASE_URL      = "https://hvtdjnbgpytncshpkxod.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2dGRqbmJncHl0bmNzaHBreG9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNDQ0NDYsImV4cCI6MjA5MjcyMDQ0Nn0._W-IkQkfQTmNZq0Rgvqortt6QJqP-FijanMtGGZMlAA";
+
